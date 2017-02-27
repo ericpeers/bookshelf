3a006d1618a271047bd279e8195dbdef4495c78c
Eric Peers <peers@mtnboy.net>
2017-02-26 21:20:40 -0700

    snapshot book listing/creation. Has an error in rating piece in adding a rating. Shall fix that shortly

---
+++ app/models/book.rb
@@ -0,0 +1,24 @@
+class Book < ApplicationRecord
+  has_many :ratings,
+           dependent: :destroy #chain deletion - if I delete my book, ratings go too.
+  validates :title, presence: true,
+            length: {minimum: 1} #C by Tom McCarthy is a legit book
+
+  validates :author, presence: true,
+            length: {minimum: 1} #assume that some author can have at least a single letter name or symbol like Prince.
+
+  validate :isbn_length
+  #this didn't work. So then I switched tactics
+  #validates :isbn, length: {in: (10 13)}, uniqueness: true
+  ##want the sequence 10,13; not the range 10..11.12.13.
+  ##https://www.tutorialspoint.com/ruby/ruby_ranges.htm
+
+  def isbn_length
+    if (! ((isbn.length == 10) || (isbn.length == 13)))
+      errors.add(:isbn, "ISBN should be either 10 or 13 characters (digits) long");
+    end
+  end
+
+
+
+end
